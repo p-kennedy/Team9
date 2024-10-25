35dd217d6135f919783b4f1e1777fea9997962d6
peter <24419788@studentmail.ul.ie>
2024-10-25 13:51:04 +0100

    Added mainfile.java

---
+++ project/mainfile.java
@@ -0,0 +1,35 @@
+public class mainfile {
+    public static void main(String[] args)
+    {
+        System.loadLibrary("sysinfo");
+        sysInfo info = new sysInfo();
+        cpuInfo cpu = new cpuInfo();
+        cpu.read(0);
+
+        showCPU();
+        showPCI();
+        showUSB();
+        showDisk();
+        showMem();
+    }
+
+    //Peter
+    public static void showCPU()
+    {}
+
+    //Aaron
+    public static void showPCI()
+    {}
+
+    //Rosie
+    public static void showUSB()
+    {}
+
+    //Waleed
+    public static void showDisk()
+    {}
+
+    //Waleed
+    public static void showMem()
+    {}
+}
