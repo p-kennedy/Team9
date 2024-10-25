9308b9e5d362c703e2b85c662a9facc5b3bef058
Sark37 <aaronodoherty7@gmail.com>
2024-10-25 12:49:50 +0000

    Changing template pci count again

--- project/template.java
+++ project/template.java
@@ -12,12 +12,12 @@ public class template
 
 
         System.out.println("\nThis machine has "+
-            pci.busCount()+" PCI buses ");
+            pci.busCount()+" PCI bus ");
 
         // Iterate through each bus
         for (int i = 0; i < pci.busCount(); i++) {
             System.out.println("Bus "+i+" has "+
-                pci.deviceCount(i)+" device");
+                pci.deviceCount(i)+" devices");
 
             // Iterate for up to 32 devices.  Not every device slot may be populated
             // so ensure at least one function before printing device information
